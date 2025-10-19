4ac7d09bf4e6fda4eecbb94a9f2365ba0aed9f2c
Leo Zhang <leoyzhang06@gmail.com>
2025-10-18 20:54:35 -0500

    added action controller functions

---
+++ backend/src/controllers/actionController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+export const uploadProblem = async (req: Request, res: Response): Promise<void> =>  {
+
+}
+
+export const sendTeacherCatMessage = async (req: Request, res: Response): Promise<void> =>  {
+
+}
+
+// Boolean for done or not, handled accordingly
+export const teacherCatDone = async (req: Request, res: Response): Promise<void> =>  {
+
+}
+
+export const sendStudentCatMessage = async (req: Request, res: Response): Promise<void> =>  {
+
+}
--- backend/src/routes/actionRoutes.ts
+++ backend/src/routes/actionRoutes.ts
@@ -1,15 +1,17 @@
+import { 
+    sendStudentCatMessage, 
+    sendTeacherCatMessage, 
+    teacherCatDone, 
+    uploadProblem 
+} from '../controllers/actionController';
 import { Router } from 'express';
 
 const router = Router();
 
-/*
-
-router.post('/actions/upload-problem', ); // Send a problem, receive first question from teacher cat
-router.post('/actions/teacher-cat-message', ); // Send teacher cat a message, receive one back
-router.post('/actions/teacher-cat-done', ); // Boolean for done or not, handled accordingly
-router.post('/actions/student-cat-message', ); // Send student cat a message, receive one back
+router.post('/actions/upload-problem', uploadProblem); // Send a problem, receive first question from teacher cat
+router.post('/actions/teacher-cat-message', sendTeacherCatMessage); // Send teacher cat a message, receive one back
+router.post('/actions/teacher-cat-done', teacherCatDone); // Boolean for done or not, handled accordingly
+router.post('/actions/student-cat-message', sendStudentCatMessage); // Send student cat a message, receive one back
                                                // If done, include flag and points earned
-
-*/
 
 export default router;
